11af41adfc890d0c4209ac010ffd2327161b33ee
Marian Volytskyi <volmark00@gmail.com>
2023-10-17 15:15:37 +0300

    add solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,20 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return true for empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should return true for isogram word', () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it('should return false for repeated chars', () => {
+    expect(isIsogram('blablacar')).toBe(false);
+  });
+
+  it('should be case-insensitive', () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
 });
